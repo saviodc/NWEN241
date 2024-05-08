dc2f5929c61e2590258eb84474a374ba5956462f
Savio Concessio <167926647+saviodc@users.noreply.github.com>
2024-05-08 13:59:02 +1200

    Add files via upload

--- NWEN241_Assignment3/files/server.c
+++ NWEN241_Assignment3/files/server.c
@@ -11,6 +11,10 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 // Include necessary header files
 
 /**
@@ -21,16 +25,47 @@
  * Read the assignment handout for more details about the server program
  * design specifications.
  */ 
+
+int initiate(int);
+int set_listen(int);
 int main(int argc, char *argv[])
 {
 
 
-    if(!isdigit(argv[1]) || argc != 2){
-        printf("Error");
+    if(argc != 2){
+        printf("Error init %d",argc);
         return -1;
     }
 
+    int port = atoi(argv[1]);
+    if(port < 1024){
+        printf("Invalid port - %d",port);
+        return -1;
+    }
+    if(!initiate(port))return -1;
 
     return 0;
+}
+
+int initiate(int port){
+    printf("Rwached port - %d\n", port);
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(fd < 0){
+        printf("Error - initiate at port %d", port);
+        return 0;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if(bind(fd, (struct sockaddr *)&addr, sizeof(addr))< 0){
+        printf("Binding error");
+        return 0;
+    }
+
+    printf("Successful bind @ %d\n\n", port);
+    return fd;
 }
 
